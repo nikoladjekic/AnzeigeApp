bd2e05d3b436ad43b8ff2a3e24b5eca27a1a749a
Nikola Djekic <nikola.dekic@eng.it>
2020-02-20 15:05:07 +0100

    add guard to check the login page

---
+++ front/src/guards/auth.guard.ts
@@ -0,0 +1,18 @@
+import { AuthService } from "../services/auth.service";
+import { Injectable } from "@angular/core";
+import { CanActivate, Router } from "@angular/router";
+
+@Injectable({
+  providedIn: "root"
+})
+export class AuthGuard implements CanActivate {
+  constructor(private _authService: AuthService, private _router: Router) {}
+
+  canActivate(): boolean {
+    if (this._authService.loggedIn()) return true;
+    else {
+      this._router.navigate(["/login"]);
+      return false;
+    }
+  }
+}
