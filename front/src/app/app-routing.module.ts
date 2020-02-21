79b6c0123a493100f8e9660770be258a7cdf4dd0
Nikola Djekic <nikola.dekic@eng.it>
2020-02-21 16:03:35 +0100

    set up sub routing for admin page

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,15 +1,28 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
+import { AuthGuard } from "src/guards/auth.guard";
+
 import { WerbungPageComponent } from "./werbung-page/werbung-page.component";
 import { AdminPageComponent } from "./admin-page/admin-page.component";
 import { LoginPageComponent } from "./login-page/login-page.component";
-import { AuthGuard } from "src/guards/auth.guard";
+import { BenutzerComponent } from "./admin-page/benutzer/benutzer.component";
+import { AnzeigenComponent } from "./admin-page/anzeigen/anzeigen.component";
 
 const routes: Routes = [
   { path: "", component: WerbungPageComponent },
-  { path: "admin", component: AdminPageComponent, canActivate: [AuthGuard] },
-  { path: "login", component: LoginPageComponent }
+  { path: "login", component: LoginPageComponent },
+  {
+    path: "admin",
+    component: AdminPageComponent,
+    canActivate: [AuthGuard],
+    children: [
+      { path: "benutzer", component: BenutzerComponent },
+      { path: "anzeigen", component: AnzeigenComponent }
+    ]
+  },
+  // wild card route, goes at the end of all routes
+  { path: "**", redirectTo: "", component: WerbungPageComponent }
 ];
 
 @NgModule({
