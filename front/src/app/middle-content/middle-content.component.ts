dc0a63f60b2c4e44f195556f9b3c454c8ed41ba3
Nikola Djekic <nikola.dekic@eng.it>
2020-02-27 16:19:23 +0100

    add dynamic render of anzeige on the homepage

--- front/src/app/middle-content/middle-content.component.ts
+++ front/src/app/middle-content/middle-content.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AnzeigeService } from 'src/services/anzeige.service';
+
 @Component({
   selector: 'app-middle-content',
   templateUrl: './middle-content.component.html',
   styleUrls: ['./middle-content.component.css']
 })
 export class MiddleContentComponent implements OnInit {
+  listOfAnzeigen = [];
 
-  constructor() { }
+  constructor(private _ad: AnzeigeService) { }
 
   ngOnInit() {
+    this._ad.getAllAnzeigen().subscribe(res => {
+      this.listOfAnzeigen = res;
+    });
   }
 
 }
