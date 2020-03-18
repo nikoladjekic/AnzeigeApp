58dc94ff87abea87d9435b3185361c065b7d12be
Nikola Djekic <nikola.dekic@eng.it>
2020-03-18 19:45:15 +0100

    add search to inactive anzeigen

--- front/src/app/admin-page/anzeigen-inaktiv/anzeigen-inaktiv.component.ts
+++ front/src/app/admin-page/anzeigen-inaktiv/anzeigen-inaktiv.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 
 import { AnzeigeService } from "src/services/anzeige.service";
+import { Bundesland } from 'src/models/bundesland.enum';
 
 @Component({
   selector: "app-anzeigen-inaktiv",
@@ -8,7 +9,23 @@ import { AnzeigeService } from "src/services/anzeige.service";
   styleUrls: ["./anzeigen-inaktiv.component.css"]
 })
 export class AnzeigenInaktivComponent implements OnInit {
+
   inaktiveAnzeigen = [];
+  filterValues = this.inaktiveAnzeigen;
+  selectedBundesland: Bundesland;
+  searchTerm: string;
+
+  bundesland: Bundesland[] = [
+    Bundesland.V,
+    Bundesland.T,
+    Bundesland.S,
+    Bundesland.OÖ,
+    Bundesland.NÖ,
+    Bundesland.W,
+    Bundesland.K,
+    Bundesland.B,
+    Bundesland.ST
+  ];
 
   constructor(private _ad: AnzeigeService) {}
 
@@ -28,4 +45,17 @@ export class AnzeigenInaktivComponent implements OnInit {
       })
     })
   }
+
+  sortByBundesland(){
+    this.inaktiveAnzeigen = this.filterValues.filter(el => {
+      return el.bundesland.toUpperCase().indexOf(this.selectedBundesland.toUpperCase()) >= 0;
+    })  
+  }
+
+  searchByName(): void {
+    this.inaktiveAnzeigen = this.filterValues.filter(el => {
+      return el.firma.toUpperCase().indexOf(this.searchTerm.toUpperCase()) >= 0;
+    })  
+  }
+
 }
