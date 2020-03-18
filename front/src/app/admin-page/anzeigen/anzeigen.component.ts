31ff448cad4012565c9c515c47dbf8ac6adc66af
Nikola Djekic <nikola.dekic@eng.it>
2020-03-18 19:27:13 +0100

    edit search options for active anzeigen

--- front/src/app/admin-page/anzeigen/anzeigen.component.ts
+++ front/src/app/admin-page/anzeigen/anzeigen.component.ts
@@ -42,19 +42,23 @@ export class AnzeigenComponent implements OnInit {
   }
 
   sortByBundesland(){
-    this._ad.getAllAnzeigen().subscribe(res => res.forEach(anzeige => {
-      if (anzeige.bundesland === this.selectedBundesland){
-        this.aktiveAnzeigen = [];
-        this.checkForDateExpiration(anzeige);
-      }             
-    }))
+    this.aktiveAnzeigen = this.filterValues.filter(el => {
+      return el.bundesland.toUpperCase().indexOf(this.selectedBundesland.toUpperCase()) >= 0;
+    })  
+  }
+
+  searchByName(): void {
+    this.aktiveAnzeigen = this.filterValues.filter(el => {
+      return el.firma.toUpperCase().indexOf(this.searchTerm.toUpperCase()) >= 0;
+    })  
   }
   
   checkForDateExpiration(val){
     let today: Date = new Date();
     let expDate: Date = new Date(val.endDate);
-    // 30*24*60*60*1000 = days*hour*min*sec*milisec
+    // set expiration threshold to 30 days :: days*hour*min*sec*milisec
     let threshold: Date = new Date(expDate.getTime()-(30*24*60*60*1000));
+    // check if ad is active
     if (expDate > today){
       this.aktiveAnzeigen.push(val);
       // check if the ad is about to expire
@@ -64,13 +68,6 @@ export class AnzeigenComponent implements OnInit {
       }
     }
   }
-
-  searchByName(): void {
-    this.aktiveAnzeigen = this.filterValues.filter(el => {
-      return el.firma.toUpperCase().indexOf(this.searchTerm.toUpperCase()) >= 0;
-    })  
-  }
-  
 
   seeDetails(val) {
     this._router.navigate(['/', { id: val }]);
