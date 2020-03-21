0ce7199d2f3a7557d52fd0305de45bd3754edda2
Nikola Djekic <nikola.dekic@eng.it>
2020-03-21 18:24:04 +0100

    add new methods for active and inactive ads

--- front/src/services/anzeige.service.ts
+++ front/src/services/anzeige.service.ts
@@ -1,23 +1,32 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 
-import { Anzeige } from 'src/models/anzeige.model';
+import { Anzeige } from "src/models/anzeige.model";
 
 @Injectable({
   providedIn: "root"
 })
 export class AnzeigeService {
-
   defUrl: string = "http://localhost:3030/api/";
 
-  private _getAllAnzeigen = `${this.defUrl}anzeigen`;
-  private _getAnzeigeById = `${this.defUrl}anzeige/details/`;
-  private _postNewAnzeige = `${this.defUrl}anzeige/add`;
+  private _getAllAnzeigen = this.defUrl + "anzeigen";
+  private _getActiveAnzeigen = this.defUrl + "anzeigen/active";
+  private _getInactiveAnzeigen = this.defUrl + "anzeigen/inactive";
+  private _getAnzeigeById = this.defUrl + "anzeige/details/";
+  private _postNewAnzeige = this.defUrl + "anzeige/add";
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getAllAnzeigen() {
     return this.http.get<any>(this._getAllAnzeigen);
+  }
+
+  getActiveAnzeigen() {
+    return this.http.get<any>(this._getActiveAnzeigen);
+  }
+
+  getInactiveAnzeigen() {
+    return this.http.get<any>(this._getInactiveAnzeigen);
   }
 
   postNewAnzeige(anzeigeValue) {
